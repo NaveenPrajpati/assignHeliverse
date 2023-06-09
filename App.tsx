f1f4fa1bfabb4e0e482037b1839fc84c1206667d
NaveenPrajapati <solidauto202@gmail.com>
2023-06-09 22:30:29 +0530

    submit time
    need some finishing

--- App.tsx
+++ App.tsx
@@ -6,21 +6,42 @@ export default function App() {
   const[screen2,setScreen2]=useState(false)
   const[screen3,setScreen3]=useState(false)
   const[closeProfile,setCloseProfile]=useState(false)
+  const[num,setNum]=useState(15)
 
-  // useEffect(()=>{
-  //   setTimeout(() => {
-  //     setScreen1(false)
-  //     setScreen2(true)
-  //   }, 3000);
-  // },[screen1])
+  useEffect(()=>{
+    const myTimeout= setTimeout(() => {
+      setScreen1(false)
+      setScreen2(true)
+      clearTimeout(myTimeout)
+    }, 3000);
+  },[screen1==true])
 
-  // useEffect(()=>{
-  //   setTimeout(() => {
-  //     setScreen2(false)
-  //     setScreen3(true)
-  //   }, 15000);
-  // },[screen2])
+  useEffect(()=>{
+    const myTimeout= setTimeout(() => {
+      setScreen2(false)
+      setScreen3(true)
+      clearTimeout(myTimeout)
+    }, 15000);
+  },[screen2===true])
 
+  useEffect(() => {
+    
+    const interval = setInterval(() => {
+      if(screen3)
+      setNum((prevNum) => {
+        if (prevNum < 40) {
+          return prevNum + 1;
+        } else {
+          clearInterval(interval);
+          return prevNum;
+        }
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [screen3===true]);
 
   //animation reference
   const animatedValue = useRef(new Animated.Value(0)).current;
@@ -31,10 +52,10 @@ export default function App() {
 
   const animate = () => {
     Animated.timing(animatedValue, {
-      toValue: 1, // The final value for the animation
-      duration: 200, // Duration of the animation in milliseconds
-      useNativeDriver: true, // Enable native driver for better performance
-    }).start(); // Start the animation
+      toValue: 1, 
+      duration: 200, 
+      useNativeDriver: true,
+    }).start(); 
   };
 
   const animstart = animatedValue.interpolate({
@@ -49,11 +70,10 @@ export default function App() {
 
   return (
     <View>
-      
       <View style={styles.container} >
-{/* 
+
         {
-          false && <View style={styles.topScreen1}>
+          screen1 && <View style={styles.topScreen1}>
       
             <Image style={{width:100,height:100,marginTop:100,transform:[{translateX:60}]}} source={require('./Assets/castingLogo.png')}></Image>
             <Text style={{color:'white',fontSize:20,transform:[{rotate:'-15deg'},{translateY:-45},{translateX:85}]}}>04:52:59</Text>
@@ -61,11 +81,11 @@ export default function App() {
             <Text style={{color:'pink',top:5,fontSize:30,transform:[{rotate:'-10deg'},{translateY:-30},{translateX:35}]}}>CASTING CALL</Text>
             <Text style={{color:'yellow',top:15,fontSize:30}}>The Results R In!</Text>
           </View>
-        } */}
+        }
         {
-          true && <View style={{position:'absolute',zIndex:2,top:50}}>
+          screen2 && <View style={{position:'absolute',zIndex:2,top:50}}>
             
-            <Animated.View style={[{backgroundColor:'green',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:45,width:'100%'}, {transform: [{translateX:animstart },{translateX:closeProfile?animstart2:0}]},]}>
+            <Animated.View style={[{flexDirection:'row',justifyContent:'center',alignItems:'center',gap:45,width:'100%'}, {transform: [{translateX:animstart },{translateX:closeProfile?animstart2:0}]},]}>
             
             <Image style={{width:100,height:100,borderRadius:50,borderWidth:2,borderColor:'yellow'}} source={require('./Assets/avtar2.png')}></Image>
             <View style={{alignItems:'center'}}>
@@ -81,11 +101,35 @@ export default function App() {
             </View>
           </View>
         }
+
+{
+          screen3 && <View style={{position:'absolute',zIndex:2,top:50}}>
+            
+            
+            
+            <Text style={{color:'yellow',top:5,fontSize:40,textAlign:'center'}}>4 Friends Gave  U {"\n"}Some Love</Text>
+            <View style={{alignItems:'center',marginTop:50 ,gap:-115}}>
+            <Image style={{width:200,height:150,}} source={require('./Assets/main-heart.png')}></Image>
+            <Text style={{color:'yellow',top:15,fontSize:40}}>{num}000</Text>
+            </View>
+          </View>
+        }
+
        
        <Image style={styles.image} source={require('./Assets/award_bg.png')}></Image>
        <View style={styles.arrowImageView}>
        <Image style={styles.image3} source={require('./Assets/girlClap.png')}></Image>
-      <TouchableOpacity style={styles.arrowImage} onPress={() => setCloseProfile(true)}>
+      <TouchableOpacity style={styles.arrowImage} onPress={() =>{
+        if(screen2)
+        setCloseProfile(true)
+        if(screen3){
+        setScreen3(false)
+        setScreen2(false)
+        setNum(15)
+        setCloseProfile(false)
+          setScreen1(true)
+        }
+        }}>
        <Image style={{width:50,height:50}} source={require('./Assets/arrow.png')}></Image>
       </TouchableOpacity>
        </View>
